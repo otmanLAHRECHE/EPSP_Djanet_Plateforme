6d72d6906f789a21a15d0b039e6dc1414346a696
Otman LAHRECHE <laotman.47@gmail.com>
2022-02-12 15:16:24 +0100

    Important updates for the authentication API

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,16 +1,6 @@
-import axios from 'axios';
-import { returnErrors } from './messages';
 
-import {
-  USER_LOADED,
-  USER_LOADING,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT_SUCCESS,
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-} from './types';
+
+
 
 
 
