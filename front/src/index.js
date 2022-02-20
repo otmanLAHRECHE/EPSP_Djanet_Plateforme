916b3d0e8a00f260f49cf23fd6703e059f67614f
Otman LAHRECHE <laotman.47@gmail.com>
2022-02-20 12:31:20 +0100

    Important updates

---
+++ front/src/index.js
@@ -0,0 +1,30 @@
+import React,{Component,Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import "./main.css";
+import {BrowserRouter, HashRouter as Router, Route, Routes} from 'react-router-dom';
+import PrivateRoute from "./components/common/private_route";
+import Dashboard from "./components/admin/dashboard";
+import Login from "./components/accounts/login";
+
+
+
+class App extends Component {
+  render() {
+    console.log("render");
+    return (
+
+        <BrowserRouter>
+            <Fragment>
+              <div>
+                <Routes>
+                  <Route exact path="/" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
+                  <Route exact path="/login" element={<Login/>} />
+                </Routes>
+              </div>
+            </Fragment>
+        </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
