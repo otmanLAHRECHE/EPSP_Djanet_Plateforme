2fdde2cac468134b95b177d26c2ba8d16f45a0f1
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-16 11:16:30 +0100

    Important updates

--- EPSP_Djanet_Plateforme/settings.py
+++ EPSP_Djanet_Plateforme/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'EPSP_Djanet_Plateforme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'epsp_djanet',
-        'USER': 'dbadmin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
